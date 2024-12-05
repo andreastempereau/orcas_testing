c4b4ccb26930498175186312c8607136c33d06c6
Andreas Tempereau <andreastempereau@gmail.com>
2024-12-05 14:00:13 -0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
     },
   },
   build: {
+    outDir: 'dist', // Output directory for the build
     rollupOptions: {
       input: 'src/main.js', // Ensure this matches your entry file
     },
